9565120a25f639db60317d6a99b8cc9d9f6414e8
unknown <mailtosarantg@gmail.com>
2025-05-06 14:58:59 +0530

    answer

--- routes/Answer.router.js
+++ routes/Answer.router.js
@@ -114,23 +114,38 @@ router.get("/rank/:courseId/:sublessonIndex", authenticate, async (req, res) => 
   }
 });
 
-router.get("/:userId",async (req, res) => {
+// router.get("/:userId",async (req, res) => {
+//     try {
+//       const { userId } = req.params;
+//       const answers = await Answer.find({ user: userId })
+
+//       if (!answers || answers.length === 0) {
+//         return res.status(404).json({ message: "No answers found for this user" });
+//       }
+//       return res.status(200).json({
+//         message: "User details fetched successfully",
+//         answers: answers
+//       });
+//     } catch (error) {
+//       console.error("Error fetching user answers:", error);
+//       return res.status(500).json({ message: "Server error" });
+//     }
+//   });
+  router.get("/:userId", async (req, res) => {
     try {
       const { userId } = req.params;
-      const answers = await Answer.find({ user: userId })
-
-      if (!answers || answers.length === 0) {
-        return res.status(404).json({ message: "No answers found for this user" });
-      }
+      const answers = await Answer.find({ user: userId });
+  
       return res.status(200).json({
-        message: "User details fetched successfully",
-        answers: answers
+        message: "User answers fetched",
+        answers: answers.length > 0 ? answers : null
       });
     } catch (error) {
       console.error("Error fetching user answers:", error);
       return res.status(500).json({ message: "Server error" });
     }
   });
+  
   
 
 // Get user-wise detailed attempts for a sublesson
